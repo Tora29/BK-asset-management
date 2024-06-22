37fe753cb17933ba707caec81445db452f641645
tora29 <nikumann.power@gmail.com>
2024-06-22 22:08:45 +0900

    first commit

---
+++ src/main/java/com/asset_management/bk/BkApplication.java
@@ -0,0 +1,13 @@
+package com.asset_management.bk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/asset_management/bk/BkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asset_management.bk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
